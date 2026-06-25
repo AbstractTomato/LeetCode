f6a6569e6207af3a7e495478aea9b3e336a41e7a
QinZhennan <qinzhennan1001@163.com>
2026-06-25 09:20:26 +0800

    leetcode代码 树的最大深度 思路:递归就好,在递归的时候,难点在于返回值需要+1.如果没有+1的话,那么返回值就为0.

---
+++ src/Tree/maxDepth.java
@@ -0,0 +1,20 @@
+package Tree;
+
+
+
+/**
+ * 给定一个二叉树 root ，返回其最大深度。
+ *
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ */
+public class maxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right);
+    }
+}
