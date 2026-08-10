ce6fe05090dbc02fe44da3dfedd8b6b64155b8d6
QinZhennan <qinzhennan1001@163.com>
2026-08-10 17:29:28 +0800

    leetcode代码 打家劫舍1 思路:经典的动态规划问题.偷这一家房子之前, 判断偷前一家的金额大还是前前一家+当前这一家的金额大. 优化思路:维护两个变量即可.

---
+++ src/DP/maxMoney1.java
@@ -0,0 +1,45 @@
+package DP;
+
+
+/**
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ *
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ */
+public class maxMoney1 {
+    public int rob(int[] nums){
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++){
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+
+        return dp[n];
+    }
+
+    public int rob2(int[] nums){
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int pre = nums[0];
+        int prePre = 0;
+
+        for (int i = 2; i <= nums.length; i++){
+            int cur = Math.max(pre, prePre + nums[i - 1]);
+            prePre = pre;
+            pre = cur;
+        }
+
+        return pre;
+    }
+}
